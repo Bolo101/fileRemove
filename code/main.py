1bafa7c5ff9b25027a443318e04f3e8c2b09c1ac
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-01-08 20:13:32 +0100

    Updated code to enable custom number of passes

--- code/main.py
+++ code/main.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
 
-import sys
 import os
+from argparse import ArgumentParser
 from secure_delete import secure_delete_file, secure_delete_directory
 
-def main():
+def main(passes, targets):
     """
     Main entry point for the secure delete tool.
     """
-    if len(sys.argv) < 2:
-        print("Usage: fr <path1> [path2 ... pathN]")
-        sys.exit(1)
-
-    targets = sys.argv[1:]
-    passes = 5
-
     for target_path in targets:
         if not os.path.exists(target_path):
             print(f"Error: {target_path} does not exist.")
@@ -29,5 +22,24 @@ def main():
 
     print("Secure deletion completed for all specified targets.")
 
+def _parse_args():
+    parser = ArgumentParser(description="Secure file eraser to run in terminal. Delete both files and directories")
+    parser.add_argument('-p',
+                        help="Number of passes to overwrite target data",
+                        type=int,
+                        default=7,
+                        required=False)
+    parser.add_argument('targets', 
+                        nargs='*',
+                        help="Files or directories to securely delete")
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    main()
+    args = _parse_args()
+
+    if not args.targets:
+        print("Error: No files or directories specified for deletion.")
+        print("Usage: fr [-p PASSES] <file1> [file2 ...] [-p PASSES]")
+        exit(1)
+
+    main(args.p, args.targets)
